290bd9dc2f204d6ec6a82c377d7a3e52480f44cb
Макс <maks88648@gmail.com>
2021-05-07 01:23:47 +0400

    Добавил логику авторизации

--- Application/Src/JavaScript/login.js
+++ Application/Src/JavaScript/login.js
@@ -1,15 +1,54 @@
 $(document).ready(function(){
-    $("#sing_in").on("submit", function(obj){
+    $('#sing_in').submit(function(obj){
         obj.preventDefault();
 
+        if(checkCorrectFields()) {
+            checkAccount();
+        }
+    });
+});
+function checkCorrectFields() {
+    correct = true;
+    var login = document.getElementById('sign_form_login');
+    var password = document.getElementById('sign_form_password');
+
+    if(login.value === '') {
+        $('#main_form_reg_name').html('Вы не ввели логин!');
+        correct = false;
+    }else if(password.value === '') {
+        $('#main_form_reg_name').html('Вы не ввели пароль!');
+        correct = false;
+    }
+    return correct;
+}
+function checkAccount() {
+    var regData = $("#sing_in").serialize();
         $.ajax({
             url: '../php/login.php',
-            method: 'post',
+            type: 'POST',
             dataType: 'html',
-            data: $(this).serialize(),
+            data: regData,
             success: function(data){
-                $('#main_form_reg_name').html(data);
+                switch (data) {
+                    case 'doesNotExist': {
+                        $('#main_form_reg_name').html('Данного аккаунта не существует!');
+                        break;
+                    }
+                    case 'wrongPassword': {
+                        $('#main_form_reg_name').html('Вы указали неверный пароль!');
+                        break;
+                    }
+                    case 'success': {
+                        $('#main_form_reg_name').html('Вход выполнен успешно!');
+                        window.location.href = '../html/game_catalog.html';
+                        break;
+                    }
+                    case 'errorConnectToBD': {
+                        $('#main_form_reg_name').html('Подключение к БД не удалось!');
+                        break;
+                    }
+                    default:  $('#main_form_reg_name').html(data);
+                }
             }
         });
-    });
-})
+}
--- Application/Src/php/login.php
+++ Application/Src/php/login.php
@@ -1,9 +1,30 @@
 <?php
 
-if (empty($_POST['sign_form_login'])) {
-    echo 'Укажите логин';
-} elseif (empty($_POST['sign_form_password'])) {
-    echo 'Укажите пароль';
-} else {
-    echo 'Авторизация...';
+$databaseConnectInfo = 'mysql:dbname=polargame;host=localhost';
+$databaseUser = 'root';
+$databasePassword = 'root';
+
+try {
+    $databaseConnect = new PDO($databaseConnectInfo, $databaseUser, $databasePassword);
+
+    $query = 'SELECT * FROM `users` WHERE `login` = :user_login';
+
+    $databaseResult = $databaseConnect->prepare($query);
+    $databaseResult->execute(['user_login'=>$_POST['sign_form_login']]);
+
+    $user = $databaseResult->fetchAll();
+
+
+    if(count($user) == 0) {
+        echo 'doesNotExist';
+    } else if ($user[0]['password'] != $_POST['sign_form_password']) {
+        echo 'wrongPassword';
+    } else {
+        echo 'success';
+    }
+
+
+
+} catch (PDOException $e) {
+    echo 'errorConnectToBD';
 }
