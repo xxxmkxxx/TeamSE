61740e18953d6a80911d8c26d701178e170693d3
Макс <maks88648@gmail.com>
2021-06-12 22:29:49 +0400

    Добавил возможность создания комментария в форме создания комментария на странице игры.

---
+++ Application/Src/JavaScript/GameCreateComment.js
@@ -0,0 +1,38 @@
+$(document).ready(mainFunction());
+
+function mainFunction() {
+    var game;
+    getGameByName(getNameGame()).done(function (data) {
+        game = $.parseJSON(data)[0];
+        getSession().done(function (data) {
+            var session = $.parseJSON(data);
+
+            createComment(game['game_id'], session['id']);
+        });
+    });
+}
+//Функция для отправки комментария в бд
+function createComment(gameId, userId) {
+    $('#write_commment_form').submit(function (obj) {
+        obj.preventDefault();
+
+        var commentText = $('#write_commment_area').val();
+
+        if(isCorrect(commentText)) {
+            $.ajax({
+                url: '../php/set_comment.php',
+                type: 'POST',
+                data: {gameId: gameId, userId: userId, commentText: commentText}
+            });
+        }
+    });   
+}
+//Функция проверки корректности информации в поле
+function isCorrect(value) {
+    var flag;
+    var isExistsFlag = value != '';
+    var isMoreNormalLengthFlag = value.length > 10;
+
+    flag = isExistsFlag && isMoreNormalLengthFlag;
+    return true;
+}
