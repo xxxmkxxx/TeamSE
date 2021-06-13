6ad9501d646486146a0b4814587b2b74d9f74e01
jekaRWS <jekarws@gmail.com>
2021-06-13 15:46:39 +0400

    modified:   Application/Src/php/get_count_party_members.php fix

--- Application/Src/php/get_count_party_members.php
+++ Application/Src/php/get_count_party_members.php
@@ -1,9 +1,9 @@
 <?php
 include('DataBaseModul.php');
 
-$query = 'SELECT * FROM COUNT(party_members) AS `count` WHERE party_id = :party_id';
+$query = 'SELECT COUNT(party_id) AS `count` FROM party_members WHERE party_id = :party_id;';
 $prepare = ["party_id" => $_POST['partyId']];
 
-$party_members = executePreparedQuery($query, $prepare);
+$count_members = executePreparedQuery($query, $prepare);
 
-echo json_encode($party_members);
+echo json_encode($count_members);
