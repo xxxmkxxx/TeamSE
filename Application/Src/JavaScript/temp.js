0784c02fed9165f35cb7606561b46540084d7090
Tatiana-Popova <Tatiana-Popova@users.noreply.github.com>
2021-06-14 02:52:35 +0400

    рабочий момент в форме пати

---
+++ Application/Src/JavaScript/temp.js
@@ -0,0 +1,36 @@
+
+// функция окрашивания border в зеленый
+function ready_nick(){
+	$(document).on('click','#ready_button', function (obj) {
+        obj.preventDefault();
+
+        $("#my_nick").css("border-color", "#76D874");
+        $('#my_nick').attr('readonly', true);
+        $("#edit_icon").css("display","flex");
+    });
+
+    $(document).on('click','#edit_icon', function (obj) {
+        obj.preventDefault();
+        $("#my_nick").css("border-color", "#2B3E8A");
+        $('#my_nick').attr('readonly', false);
+         $("#edit_icon").css("display","none");
+    });
+
+    $(document).on('click','#exit_button', function (obj) {
+        obj.preventDefault();
+
+        $("#party_form").hide();
+    });
+
+
+}
+
+
+
+
+
+
+
+
+
+ready_nick();
